e13aacc21854897cde7f385930ddd5e3212cc13e
Derek Neighbors <derek@integrumtech.com>
2013-08-16 15:59:07 -0700

    Clean Up Single Page

--- single.php
+++ single.php
@@ -8,7 +8,7 @@
 
 get_header(); ?>
 <div class="row-fluid">
-	<div class="span12">
+	<div class="span7">
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php get_template_part( 'content', 'single' ); ?>
@@ -23,5 +23,46 @@ get_header(); ?>
 
 			<?php endwhile; // end of the loop. ?>
 	</div>
+	<div class="span5 pull-right">
+		  <p class="text-center">
+				<a href="http://gangplankhq.com/get-involved">
+					<button class="btn btn-large btn-block btn-success" type="button">Get Involved!</button>
+				</a>
+			</p>
+
+
+	    <div class="well">
+	      <h3>Upcoming Events</h3>
+	      <?php
+	        $em_categories = new EM_Categories();
+	        $em_category_ids = array();
+	        $em_neg_category_ids = array();
+	        foreach ($em_categories->get() as $em_category) {
+	          if (in_array($em_category->name, $category_names)) {
+	            $em_category_ids[] = $em_category->id;
+	          } else {
+	            $em_neg_category_ids[] = '-' . $em_category->id;
+	          }
+	        }
+	        $em_category_ids = array(join(',', $em_category_ids), join(',', $em_neg_category_ids));
+	      ?>
+	      <?php $em_events = new EM_Events(); echo $em_events->output(array('category' => join(',', $em_category_ids), 'format_header' => '', 'format' => '<div class="row-fluid event"><div class="span4 date">#_{m/d h:ia} #@_{-<br/> m/d h:ia}</div><div class="span8">#_EVENTLINK{has_room}<br/>#_ATT{room} {/has_room}<br/>#_EVENTEXCERPT</div></div>')); ?>
+	      <div class="row-fluid">
+	        <div class="span12">
+	          <br/>
+	          <a href="" class="pull-right">More Events...</a>
+	        </div>
+	      </div>
+	    </div>
+	</div>
+
+
+
 </div>
+
+
+
+
+
+
 <?php get_footer(); ?>
