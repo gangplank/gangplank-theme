ee740b2fc98651514ad4e8158d99f40c008f66cc
Derek Neighbors <derek@integrumtech.com>
2013-08-16 16:34:02 -0700

    changes to byline

--- single.php
+++ single.php
@@ -12,8 +12,8 @@ get_header(); ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php get_template_part( 'content', 'single' ); ?>
-
 				<?php _straps_content_nav( 'nav-below' ); ?>
+
 
 				<?php
 					// If comments are open or we have at least one comment, load up the comment template
