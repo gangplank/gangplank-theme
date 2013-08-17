3cc7d85c5e5194246111ffe9bbee7998ce2ed49a
Derek Neighbors <derek@integrumtech.com>
2013-08-16 17:13:02 -0700

    Add locations to side bar for single

--- single.php
+++ single.php
@@ -30,7 +30,17 @@ get_header(); ?>
 				</a>
 			</p>
 
-
+	    <div class="well">
+	      <h3>Locations</h3>
+				<h4><a href="../avondale">Avondale, AZ</a></h4>
+				<h4><a href="../chandler">Chandler, AZ</a></h4>
+				<h4><a href="../richmond">Richmond, VA</a></h4>
+				<h4><a href="../sault">Sault St. Marie, ON</a></h4>
+				<h4><a href="../tucson">Tucson, AZ</a></h4>
+				<h4><a href="../start">Start</a> a Gangplank in your city!</h4>
+			</div>
+			
+			
 	    <div class="well">
 	      <h3>Upcoming Events</h3>
 	      <?php
