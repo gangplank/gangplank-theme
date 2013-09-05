5c9bfe4a757de916fb39cb3c2d1a8fdcdcba6537
iwantdavid <github@allstruck.com>
2013-09-05 03:15:02 -0700

    Added Foundation includes from CloudFlare CDN.

--- mobile_form.php
+++ mobile_form.php
@@ -18,6 +18,9 @@
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 <![endif]-->
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/foundation/4.2.3/css/normalize.min.css">
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/foundation/4.2.3/css/foundation.min.css">
+<script src="//cdnjs.cloudflare.com/ajax/libs/foundation/4.2.3/js/foundation.min.js"></script>
 </head>
 
 <body <?php body_class(); ?>>
