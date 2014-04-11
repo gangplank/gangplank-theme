d437a8b066f4c48e030b6ef800606de821f861c9
David Monaghan <github@allstruck.com>
2014-04-11 11:42:32 -0700

    Removed Tuscon link from sidebar on index, etc.

--- index.php
+++ index.php
@@ -48,12 +48,11 @@ get_header(); ?>
 
 	    <div class="well">
 	      <h3>Locations</h3>
-				<h4><a href="../avondale">Avondale, AZ</a></h4>
-				<h4><a href="../chandler">Chandler, AZ</a></h4>
-				<h4><a href="../richmond">Richmond, VA</a></h4>
-				<h4><a href="../sault">Sault St. Marie, ON</a></h4>
-				<h4><a href="../tucson">Tucson, AZ</a></h4>
-				<h4><a href="../start">Start</a> a Gangplank in your city!</h4>
+				<h4><a href="../avondale/">Avondale, AZ</a></h4>
+				<h4><a href="../chandler/">Chandler, AZ</a></h4>
+				<h4><a href="../richmond/">Richmond, VA</a></h4>
+				<h4><a href="../sault/">Sault St. Marie, ON</a></h4>
+				<h4><a href="../start/">Start</a> a Gangplank in your city!</h4>
 			</div>
 			
 			
